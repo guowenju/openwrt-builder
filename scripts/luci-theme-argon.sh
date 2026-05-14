4120196f22d6c7bba2d01b51becc52923347d588
gwj <sfwwslm@gmail.com>
2026-05-15 06:44:48 +0800

    update repository URL for luci-theme-argon

--- scripts/luci-theme-argon.sh
+++ scripts/luci-theme-argon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git
+git clone --depth 1 https://github.com/guowenju/luci-theme-argon.git
 rm -rf package/feeds/luci/luci-theme-argon
 mv luci-theme-argon package/feeds/luci/luci-theme-argon
